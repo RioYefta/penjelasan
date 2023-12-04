68862cfe39729ec0f9498b495b24f251d26ae8c3
RecipeRevelation <reciperevelation23@gmail.com>
2023-12-04 18:59:31 +0800

    new

--- Movies/Popular.jsx
+++ Movies/Popular.jsx
@@ -20,49 +20,49 @@ import Select from "react-select";
 // Definisi komponen utama bernama "Popular"
 const Popular = () => {
   // Menggunakan hook useState untuk menyimpan nilai pageNum dan fungsi setPageNum
-  const [pageNum, setPageNum] = useState(1);
+  const [pageNum, setPageNum] = useState(1); // State ini digunakan untuk menyimpan halaman saat ini yang sedang ditampilkan.
 
   // Menggunakan hook useState untuk menyimpan nilai allMovies dan fungsi setAllMovies
-  const [allMovies, setAllMovies] = useState([]);
+  const [allMovies, setAllMovies] = useState([]); // State ini digunakan untuk menyimpan semua film yang telah diambil dari API.
 
   // Menggunakan hook useState untuk menyimpan nilai genre dan fungsi setGenre
-  const [genre, setGenre] = useState(null);
+  const [genre, setGenre] = useState(null); // State ini digunakan untuk menyimpan genre film yang sedang dipilih.
 
   // Mendefinisikan konstanta media_type dengan nilai "movie"
-  const media_type = "movie";
+  const media_type = "movie"; // Variabel ini digunakan sebagai parameter untuk mengambil data genre.
 
   // Menggunakan hook useGetPopularMovieQuery untuk mendapatkan data film populer
-  const { data: movies, isFetching, error } = useGetPopularMovieQuery(pageNum);
+  const { data: movies, isFetching, error } = useGetPopularMovieQuery(pageNum); // Hasil dari query disimpan dalam variabel movies, sedangkan isFetching menunjukkan apakah query sedang berlangsung, dan error berisi pesan error jika terjadi.
 
   // Menggunakan hook useGetGenresDataQuery untuk mendapatkan data genre
-  const { data: genresData } = useGetGenresDataQuery(media_type);
+  const { data: genresData } = useGetGenresDataQuery(media_type); // Hasil dari query disimpan dalam variabel genresData.
 
-  // Menggunakan useEffect untuk menjalankan logika setelah data film populer diambil
+  // useEffect digunakan untuk melakukan efek samping setelah render komponen. Dalam hal ini, efek sampingnya adalah memperbarui state allMovies ketika nilai movies berubah.
   useEffect(() => {
     if (movies?.results) {
-      // Menambahkan hasil film ke array allMovies
+      // Memeriksa apakah ada hasil (results) dari data yang diterima dari query useGetPopularMovieQuery. Ini dilakukan untuk menghindari kesalahan (undefined) jika data belum diterima.
       setAllMovies((prevMovies) => [...prevMovies, ...movies.results]);
-    }
+    } // Memperbarui state allMovies dengan menggabungkan hasil sebelumnya dengan hasil baru.
   }, [movies]);
 
-  // Fungsi untuk mengambil halaman berikutnya dari film populer
+  // Fungsi yang bertujuan untuk memuat halaman berikutnya dari film populer. Saat dipanggil, fungsi ini menggunakan setPageNum untuk meningkatkan nilai pageNum (halaman saat ini) dengan 1, sehingga memicu pengambilan data film untuk halaman selanjutnya saat komponen dire-render.
   const fetchNextPage = () => {
     setPageNum((prevPageNum) => prevPageNum + 1);
   };
 
-  // Fungsi yang dijalankan ketika pemilihan genre berubah
+  // Mendefinisikan fungsi onChange, yang digunakan untuk menangani perubahan dalam pemilihan genre film
   const onChange = (selectedOptions) => {
     setGenre(selectedOptions);
 
-    // Mendapatkan id-genre yang dipilih
-    const selectedGenreIds = selectedOptions.map((option) => option.id);
+    //  Memperbarui state genre dengan nilai dari selectedOptions. selectedOptions adalah array objek yang mewakili genre yang dipilih oleh pengguna.
+    const selectedGenreIds = selectedOptions.map((option) => option.id); // Membuat array selectedGenreIds yang berisi ID genre dari opsi yang dipilih oleh pengguna.
 
     // Memfilter film berdasarkan genre yang dipilih
     if (selectedGenreIds.length === 0) {
-      // Menampilkan semua film ketika tidak ada genre yang dipilih
+      // Jika tidak ada genre yang dipilih (selectedGenreIds.length === 0), maka semua film ditampilkan (setAllMovies(movies?.results)).
       setAllMovies(movies?.results);
     } else {
-      // Memfilter film ketika genre dipilih
+      // Jika ada genre yang dipilih, maka film-film akan difilter berdasarkan genre yang dipilih. filteredMovies akan berisi film-film yang memiliki setidaknya satu ID genre yang cocok dengan genre yang dipilih oleh pengguna.
       const filteredMovies = movies?.results.filter((movie) =>
         movie.genre_ids.some((genreId) => selectedGenreIds.includes(genreId))
       );
@@ -70,11 +70,11 @@ const Popular = () => {
     }
   };
 
-  // Mengatur pageNum kembali ke 1 ketika genre dibersihkan
+  // useEffect digunakan untuk melakukan efek samping setelah render komponen. Efek samping yang diinginkan di sini adalah mereset nilai pageNum ketika genre dihapus.
   useEffect(() => {
     if (!genre || genre.length === 0) {
       setPageNum(1);
-    }
+    } // Pengecekan apakah genre tidak ada atau panjangnya 0 (tidak ada genre yang dipilih). Jika tidak ada genre (!genre) atau panjangnya 0 (genre.length === 0), maka nilai pageNum direset menjadi 1.
   }, [genre]);
 
   // Fungsi untuk menampilkan efek loading (skeleton) selama proses pengambilan data
